88a718201705d79d177ba3f15083bb4d5f9651f4
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-08-06 15:25:58 -0400

    got the nav dropdown to work correctly and setting up for animation of navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Home extends Component {
+  state = {
+    navShown: true
+  }
+
   render() {
     return (
       <section className="splash-screen">
@@ -9,7 +13,10 @@ class Home extends Component {
           <header>
             James Dean <span>Does</span> Other Stuff
           </header>
-          <nav>
+          <button onClick={() => this.setState({ navShown: !this.state.navShown })}>
+            {this.state.navShown ? <i className="fas fa-bars" /> : <i className="fas fa-times" />}
+          </button>
+          <nav className={`nav-dropdown ${this.state.navShown ? 'is-shown' : ''} `}>
             <ol>
               <Link to="/photos">
                 <li>Gallery</li>
