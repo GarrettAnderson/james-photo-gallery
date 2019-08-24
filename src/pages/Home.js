fc27f69ba4c831f7f43c83b8fe0e4cd8bf83f9b7
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-08-23 22:39:31 -0400

    working on getting the site design down

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,28 +8,35 @@ class Home extends Component {
 
   render() {
     return (
-      <main>
-        <button className="home-nav-hamburger" onClick={() => this.setState({ navShown: !this.state.navShown })}>
-          {this.state.navShown ? <i className="fas fa-bars" /> : <i className="fas fa-times" />}
-        </button>
-        <nav className={`home-nav-dropdown ${this.state.navShown ? 'is-shown' : ''} `}>
-          <ol>
-            <Link to="/">
-              <li>Home</li>
-            </Link>
-            <Link to="/gallery">
-              <li>Gallery</li>
-            </Link>
-            <Link to="/contact">
-              <li>Contact</li>
-            </Link>
-            <Link to="/about">
-              <li>About</li>
-            </Link>
-          </ol>
-        </nav>
-        <section className="splash-screen" />
-      </main>
+      <section>
+        <main>
+          <button className="home-nav-hamburger" onClick={() => this.setState({ navShown: !this.state.navShown })}>
+            {this.state.navShown ? <i className="fas fa-bars" /> : <i className="fas fa-times" />}
+          </button>
+          <nav className={`home-nav-dropdown ${this.state.navShown ? 'is-shown' : ''} `}>
+            <ol>
+              <Link to="/">
+                <li>Home</li>
+              </Link>
+              <Link to="/gallery">
+                <li>Gallery</li>
+              </Link>
+              <Link to="/contact">
+                <li>Contact</li>
+              </Link>
+              <Link to="/about">
+                <li>About</li>
+              </Link>
+            </ol>
+          </nav>
+          <section className="splash-screen" />
+        </main>
+        <footer>
+          <h3>
+            <a href="https://www.instagram.com/jamesdeandoesotherstuff">James Dean Does Other Stuff</a>
+          </h3>
+        </footer>
+      </section>
     )
   }
 }
