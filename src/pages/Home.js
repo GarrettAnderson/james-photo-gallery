5d3dab020454400a186ba94b9eef56fbdcb39358
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-03-12 17:13:43 -0400

    added section tag around the button and nav bar element. working on getting the nav bar to transition in and out without moving the content down

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,25 +11,27 @@ class Home extends Component {
     return (
       <section className="content-container">
         <main>
-          <button className="home-nav-hamburger" onClick={() => this.setState({ navShown: !this.state.navShown })}>
-            {this.state.navShown ? <i className="fas fa-bars" /> : <i className="fas fa-times" />}
-          </button>
-          <nav className={`home-nav-dropdown ${this.state.navShown ? 'is-shown' : ''} `}>
-            <ol>
-              <Link to="/">
-                <li>Home</li>
-              </Link>
-              <Link to="/gallery">
-                <li>Gallery</li>
-              </Link>
-              <Link to="/contact">
-                <li>Contact</li>
-              </Link>
-              <Link to="/about">
-                <li>About</li>
-              </Link>
-            </ol>
-          </nav>
+          <section className="nav-section">
+            <button className="home-nav-hamburger" onClick={() => this.setState({ navShown: !this.state.navShown })}>
+              {this.state.navShown ? <i className="fas fa-bars" /> : <i className="fas fa-times" />}
+            </button>
+            <nav className={`home-nav-dropdown ${this.state.navShown ? 'is-shown' : ''} `}>
+              <ol>
+                <Link to="/">
+                  <li>Home</li>
+                </Link>
+                <Link to="/gallery">
+                  <li>Gallery</li>
+                </Link>
+                <Link to="/contact">
+                  <li>Contact</li>
+                </Link>
+                <Link to="/about">
+                  <li>About</li>
+                </Link>
+              </ol>
+            </nav>
+          </section>
           <section className="splash-screen">
             <img src={NamesakeSplash} />
           </section>
