7a31631750ff4d742662c5885dbe10795eec8462
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-03-16 16:19:38 -0400

    cleaned up some commented out code on the sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,27 +26,6 @@ export default class Sidebar extends Component {
   render() {
     return <div className="sidebar-container">
       {this.renderSidebar()}
-      {/* <section className="nav-section">
-            <button className="home-nav-hamburger" onClick={() => this.setState({ navShown: !this.state.navShown })}>
-              {this.state.navShown ? <i className="fas fa-bars" /> : <i className="fas fa-times" />}
-            </button>
-            <nav className={`home-nav-dropdown ${this.state.navShown ? 'is-shown' : ''} `}>
-              <ol>
-                <Link to="/">
-                  <li>Home</li>
-                </Link>
-                <Link to="/gallery">
-                  <li>Gallery</li>
-                </Link>
-                <Link to="/contact">
-                  <li>Contact</li>
-                </Link>
-                <Link to="/about">
-                  <li>About</li>
-                </Link>
-              </ol>
-            </nav>
-          </section> */}
     </div>
   }
 }
