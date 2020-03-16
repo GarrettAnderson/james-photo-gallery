64c68f3151aaf7e7a15e835e4271fd0d5c67dbc6
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-03-16 16:17:54 -0400

    working on the sidebar component and added css for the sidebar list items.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import NamesakeSplash from '../images/NamesakeSplash.png'
 import Sidebar from '../components/Sidebar'
 import SplashScreen from '../components/SplashScreen'
