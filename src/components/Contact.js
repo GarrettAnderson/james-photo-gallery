ede86228641ec470463a7e6388c69efed5f72b02
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-03-23 15:44:35 -0400

    added class name to conact send form. extended the width of the input fields on the contact form and added margin top to the input and textarea

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,11 +21,11 @@ class Contact extends Component {
               method="POST"
             >
               <fieldset id="fs-form-inputs">
-                <label htmlFor="full-name">Full Name</label>
+                {/* <label htmlFor="full-name">Full Name</label> */}
                 <input type="text" name="name" id="full-name" placeholder="First and Last Name" required="" />
-                <label htmlFor="email-address">Email Address</label>
+                {/* <label htmlFor="email-address">Email Address</label> */}
                 <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="" />
-                <label htmlFor="message">Message</label>
+                {/* <label htmlFor="message">Message</label> */}
                 <textarea
                   rows="5"
                   name="message"
@@ -35,7 +35,7 @@ class Contact extends Component {
                 />
                 <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
               </fieldset>
-              <input type="submit" value="send" />
+              <input className="contact-send" type="submit" value="send" />
             </form>
           </section>
         </main>
