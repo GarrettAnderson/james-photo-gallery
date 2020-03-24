e735dcc30b72e2aa10a221a3c10936811c15986e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-03-24 17:17:59 -0400

    added class name to the about me main element. added background image to the about me page

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,11 @@ class About extends Component {
 
   render() {
     return (
-      <main>
-          <Sidebar/>
+      <main className="about-me-container">
+        <Sidebar/>
         <article id="about-me">
           <h3>About</h3>
-          <p>A Florida native with a global mindset and a passion for building beautiful things.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac arcu ex. Aenean vel mauris vel eros vehicula lobortis. Pellentesque dapibus odio tristique nibh pulvinar interdum. Fusce mollis id lacus at elementum. Vestibulum eleifend fermentum lacus, eget porta nulla malesuada sed. Donec fringilla vulputate tellus vel volutpat. Aenean tincidunt eros eget massa sollicitudin, ut pellentesque augue tincidunt. Suspendisse quis mollis dolor. Duis et tristique turpis. Nam posuere justo ligula, at mattis lorem iaculis ut. Donec quis libero quis ex venenatis suscipit. Sed id porta urna. Phasellus nec libero eros. Maecenas elit nisl, tempor non maximus eget, tempus finibus magna. Sed eu dui nunc. Curabitur vitae dignissim est. Aliquam facilisis nibh turpis, sit amet feugiat orci egestas eu. Nulla molestie odio tortor, nec consequat ligula efficitur id. In ac risus nec diam cursus commodo. Cras ac nibh at felis sodales aliquam. Praesent eu neque rutrum, placerat mauris a, pulvinar tortor. Aliquam iaculis feugiat convallis. Pellentesque ac elementum lacus.</p>
         </article>
       </main>
     )
