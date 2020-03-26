35b7cfb45d07fd764a26410d9d1cf833cc310537
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-03-26 16:29:42 -0400

    removed a portion of the lorem ipsum placeholder text from the about section

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ class About extends Component {
         <Sidebar/>
         <article id="about-me">
           <h3>About</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac arcu ex. Aenean vel mauris vel eros vehicula lobortis. Pellentesque dapibus odio tristique nibh pulvinar interdum. Fusce mollis id lacus at elementum. Vestibulum eleifend fermentum lacus, eget porta nulla malesuada sed. Donec fringilla vulputate tellus vel volutpat. Aenean tincidunt eros eget massa sollicitudin, ut pellentesque augue tincidunt. Suspendisse quis mollis dolor. Duis et tristique turpis. Nam posuere justo ligula, at mattis lorem iaculis ut. Donec quis libero quis ex venenatis suscipit. Sed id porta urna. Phasellus nec libero eros. Maecenas elit nisl, tempor non maximus eget, tempus finibus magna. Sed eu dui nunc. Curabitur vitae dignissim est. Aliquam facilisis nibh turpis, sit amet feugiat orci egestas eu. Nulla molestie odio tortor, nec consequat ligula efficitur id. In ac risus nec diam cursus commodo. Cras ac nibh at felis sodales aliquam. Praesent eu neque rutrum, placerat mauris a, pulvinar tortor. Aliquam iaculis feugiat convallis. Pellentesque ac elementum lacus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac arcu ex. Aenean vel mauris vel eros vehicula lobortis. Pellentesque dapibus odio tristique nibh pulvinar interdum. Fusce mollis id lacus at elementum. Vestibulum eleifend fermentum lacus, eget porta nulla malesuada sed. Donec fringilla vulputate tellus vel volutpat. Aenean tincidunt eros eget massa sollicitudin, ut pellentesque augue tincidunt. Suspendisse quis mollis dolor. Duis et tristique turpis. Nam posuere justo ligula, at mattis lorem iaculis ut.</p>
         </article>
       </main>
     )
